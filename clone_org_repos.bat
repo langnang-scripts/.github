7364b8d7d21414ca827fc3b687c936c12e35ae54
langnang <langnang.chen@outlook.com>
2025-04-21 14:35:18 +0800

    2025-04-21 14:35:16

--- clone_org_repos.bat
+++ clone_org_repos.bat
@@ -1,9 +1,12 @@
-#!/bin/bash
+@echo off 
 
-# Batch
+:: Jump to the directory where this file reside.
+cd /d "%~dp0"
+
+:: Batch
 git clone git@github.com:langnang-scripts/bat.ffmpeg.git bat/ffmpeg
 git clone git@github.com:langnang-scripts/bat.g-i-t.git bat/g-i-t
 git clone git@github.com:langnang-scripts/bat.windows.git bat/windows
 
-# VBScript
+:: VBScript
 git clone git@github.com:langnang-scripts/vbs.windows.git vbs/windows
