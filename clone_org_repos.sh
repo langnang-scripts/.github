ee2f6fe3610f2ddc84961b2134698ef1dfdf2906
langnang <langnang.chen@outlook.com>
2025-04-21 14:16:27 +0800

    2025-04-21 14:16:25

---
+++ clone_org_repos.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Batch
+git clone git@github.com:langnang-scripts/bat.ffmpeg.git bat/ffmpeg
+git clone git@github.com:langnang-scripts/bat.g-i-t.git bat/g-i-t
+git clone git@github.com:langnang-scripts/bat.windows.git bat/windows
+
+# VBScript
+git clone git@github.com:langnang-scripts/vbs.windows.git vbs/windows
